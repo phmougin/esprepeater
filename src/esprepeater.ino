06dc44f779f229aa84b40ef60d457eef7cb14146
phmougin <phmdev25680@gmail.com>
2026-03-20 17:13:23 +0100

    line input

--- src/esprepeater.ino
+++ src/esprepeater.ino
@@ -28,7 +28,8 @@
 // record lenght in ms (to be adjusted according to the memory available on your board)    
 #define RECORD_MS  5000   
 // input jack (see board reference)
-#define INPUT_LINE ADC_INPUT_LINE1
+//LINE1: mic  LINE2: input jack  
+#define INPUT_LINE ADC_INPUT_LINE2
 
 // Audiokit ESP32 board type
 // See https://github.com/pschatzmann/arduino-audio-driver/blob/main/src/AudioBoard.h for available boards and how to define your own
